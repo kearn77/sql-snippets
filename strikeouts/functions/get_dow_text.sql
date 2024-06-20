91b31d0cc8eeac3ddb0749e6d5a674546b53ffec
kearn77 <139278063+kearn77@users.noreply.github.com>
2024-06-20 11:03:59 -0400

    Removed redundant variable from get_dow_text().

--- strikeouts/functions/get_dow_text.sql
+++ strikeouts/functions/get_dow_text.sql
@@ -3,7 +3,6 @@ Get the current day of the week as text.  Takes a date as an argument.
  */
 CREATE OR REPLACE FUNCTION get_dow_text(i DATE) RETURNS TEXT AS $$
   DECLARE
-    day_text TEXT;
     day_array TEXT[] := '{"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"}';
   BEGIN
     RETURN (SELECT day_array[EXTRACT(dow FROM i)]);
@@ -15,7 +14,6 @@ Get the current day of the week as text.  Takes an int as an argument.
  */
 CREATE OR REPLACE FUNCTION get_dow_text(i INT) RETURNS TEXT AS $$
   DECLARE
-    day_text TEXT;
     day_array TEXT[] := '{"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"}';
   BEGIN
     RETURN (SELECT day_array[i]);
